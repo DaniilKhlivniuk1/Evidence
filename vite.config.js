a8877da3c3c9c3d7f152a09de63e304d987a3830
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2025-02-01 18:38:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-import { defineConfig } from "vite";
-import glob from "glob";
-import injectHTML from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
-
 export default defineConfig(({ command }) => {
   return {
+    base: "/Evidence/", // Добавь это!
     define: {
       [command === "serve" ? "global" : "_global"]: {},
     },
     root: "src",
     build: {
       sourcemap: true,
-
       rollupOptions: {
         input: glob.sync("./src/*.html"),
         output: {
